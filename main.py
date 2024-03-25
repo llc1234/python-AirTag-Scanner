2fc4e61ec23b56896d3c097e60deb842325b8dd9
llc1234 <93080319+llc1234@users.noreply.github.com>
2024-03-25 14:07:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import simplepyble
 
+
+data_client = []
+
 APPLE_MAN_ID = "0x4C"
 FINDMY_DATA = bytes([0x12, 0x19])
 AIR_TAG_STATUSES = bytes([0x10, 0x50, 0x90, 0xd0])
@@ -8,13 +11,22 @@ adapter = simplepyble.Adapter.get_adapters()[0]
 
 while True:
     adapter.scan_for(2000)
+
     for peripheral in adapter.scan_get_results():
         manufacturer_data = peripheral.manufacturer_data()
         for manufacturer_id, value in manufacturer_data.items():
-            # print("ID:", manufacturer_id, ", value:", value, "AirTag")
             if manufacturer_id == int(APPLE_MAN_ID, 16):
                 if value.startswith(FINDMY_DATA):
                     status_byte = value[2:3]
                     if status_byte in AIR_TAG_STATUSES:
                         rssi = peripheral.rssi()
-                        print(f"{value.hex().upper()} - Found AirTag, rssi: {rssi}")
+                        byte = value.hex().upper()
+
+                        i_n = False
+                        for pp in data_client:
+                            if pp["bytes"] == byte:
+                                i_n = True
+
+                        if not i_n:
+                            data_client.append({"bytes": byte, "name": "the_name"})
+                            print(f"{byte} - Found AirTag, rssi: {rssi}")
